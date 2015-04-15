fdab15ca3ecd779482de8c888b7c689715c5c54f
Steve Hanson <s.hanson5@gmail.com>
2015-04-15 18:30:06 -0500

    Working with two files now

--- index.ios.js
+++ index.ios.js
@@ -16,104 +16,35 @@ var {
   Image,
   StyleSheet,
   ListView,
+  NavigatorIOS,
+  TabBarIOS,
   Text,
   View
 } = React;
 
+var ListScreen = require('./ListScreen');
+
 var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
 
+
 var Treats = React.createClass({
-  getInitialState: function() {
-    return {
-      dataSource: new ListView.DataSource({
-        rowHasChanged: (row1, row2) => row1 !== row2,
-      }),
-      loaded: false,
-    };
-  },
-
-  componentDidMount: function() {
-    this.fetchData();
-  },
-
-  fetchData: function() {
-    fetch(REQUEST_URL)
-      .then((response) => response.json())
-      .then((responseData) => {
-        this.setState({
-          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
-          loaded: true,
-        });
-      })
-      .done();
-  },
-
   render: function() {
-    if (!this.state.loaded) {
-      return this.renderLoadingView();
-    }
-
     return (
-      <ListView dataSource={this.state.dataSource} renderRow={this.renderMovie}
-        style={styles.listView} />
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'List',
+          component: ListScreen,
+        }}
+      />
     )
-  },
-
-  renderLoadingView: function() {
-    return (
-      <View style={styles.container}>
-        <Text>
-          Loading movies...
-        </Text>
-      </View>
-    );
-  },
-
-  renderMovie: function(movie) {
-    return (
-      <View style={styles.container}>
-        <Image
-          source={{uri: movie.posters.thumbnail}}
-          style={styles.thumbnail}
-        />
-        <View style={styles.rightContainer}>
-          <Text style={styles.title}>{movie.title}</Text>
-          <Text style={styles.year}>{movie.year}</Text>
-        </View>
-      </View>
-    );
-  },
+  }
 });
 
-var styles = StyleSheet.create({
+var styles = {
   container: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  rightContainer: {
     flex: 1
-  },
-  listView: {
-    paddingTop: 20,
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  thumbnail: {
-    width: 53,
-    height: 81,
-  },
-});
+  }
+}
 
 AppRegistry.registerComponent('Treats', () => Treats);
